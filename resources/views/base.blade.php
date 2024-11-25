47eddc5443ab9a2f5bc8f2b81ef1888ef0be4980
Théo Killian <theo.killian5700@Gmail.com>
2024-11-25 09:04:27 +0100

    Base web a push

---
+++ resources/views/base.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        {{ trim($__env->yieldContent('title')) ? trim($__env->yieldContent('title')) . ' - ' : '' }}Nxtransfert
+    </title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
+    <!-- SEO -->
+    <meta name="robots" content="index, follow"> 
+    <meta name="description" content="Nxtransfert envoyez jusqu'a 50 Go - Gratuit et sans inscrition - Gardez vos transfert pendant 30 jours">
+
+    <meta property="og:title" content="{{ trim($__env->yieldContent('title')) ? trim($__env->yieldContent('title')) . ' - ' : '' }}Nxtransfert">
+    <meta property="og:description" content="Nxtransfert envoyez jusqu'a 50 Go - Gratuit et sans inscrition - Gardez vos transfert pendant 30 jours">
+    <meta property="og:image" content="{{asset('img/logo.png')}}">
+    <meta property="og:url" content="{{ request()->url() }}">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Nxtransfert">
+    <meta name="og:autor" content="Nxtransfert">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@NexHostFR">
+    <meta name="twitter:creator" content="@NexHostFR">
+    <meta name="twitter:title" content="@yield('title', "") - Nxtransfert">
+    <meta name="twitter:description" content="Nxtransfert envoyez jusqu'a 50 Go - Gratuit et sans inscrition - Gardez vos transfert pendant 30 jours">
+    <meta name="twitter:image" content="{{asset('img/logo.png')}}">
+</head>
+<body>
+    @include('partials.header')
+
+    @yield('body')
+
+    @include('partials.footer')
+</body>
+</html>
